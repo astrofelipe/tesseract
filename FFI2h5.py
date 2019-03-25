43b2f425a809b086938361d4c6fcb315c78fb072
astrofelipe <felipe.irh@gmail.com>
2019-03-25 15:41:09 -0300

    Update FFI2h5.py

--- FFI2h5.py
+++ FFI2h5.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='FFIs to single h5 file')
 parser.add_argument('Folder', type=str)
 parser.add_argument('Camera', type=int)
 parser.add_argument('Chip', type=int)
+args = parser.parse_args()
 
 files = glob.glob(args.Folder + '*%d-%d*.fits' % (args.Camera, args.Chip))
 
