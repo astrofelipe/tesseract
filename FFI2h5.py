1d7553420d6b7e0ae353b8082c83c35ef283ca8f
astrofelipe <felipe.irh@gmail.com>
2019-03-25 16:53:49 -0300

    Update FFI2h5.py

--- FFI2h5.py
+++ FFI2h5.py
@@ -3,6 +3,7 @@ import h5py
 import glob
 import argparse
 import numpy as np
+from joblib import Parallel, delayed
 from astropy.io import fits
 from tqdm import tqdm
 
@@ -11,11 +12,13 @@ parser.add_argument('Folder', type=str)
 parser.add_argument('Sector', type=int)
 parser.add_argument('Camera', type=int)
 parser.add_argument('Chip', type=int)
+parser.add_argument('--ncpu', type=int, default=10)
 args = parser.parse_args()
 
 files  = np.sort(glob.glob(args.Folder + '*%d-%d*.fits' % (args.Camera, args.Chip)))
 nfiles = len(files)
 
+'''
 for i,f in enumerate(tqdm(files)):
     dat = fits.getdata(f)
 
@@ -26,6 +29,14 @@ for i,f in enumerate(tqdm(files)):
 
     dset[i] = dat
     del dat
+'''
+nx, ny = fits.getdata(f[0]).shape
+
+output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w')
+dset   = output.create_dataset('FFIs', (nfiles, nx, ny), dtype='f', compression='gzip')
+
+dset[:] = Parallel(n_jobs=args.ncpu)(delayed(fits.getdata)(f) for f in tqdm(files))
+
 
 print(dset)
 dset.flush()
