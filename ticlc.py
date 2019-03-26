4f344a19fa5712dbd384a0e472caa82ceca64590
astrofelipe <felipe.irh@gmail.com>
2019-03-26 17:55:26 -0300

    Update ticlc.py

--- ticlc.py
+++ ticlc.py
@@ -71,15 +71,15 @@ ccd = ccd[0]
 
 coord = SkyCoord(ra, dec, unit='deg')
 
+'''
 #if args.everest:
-    '''
     from sklearn.neighbors import KDTree
     X    = np.transpose([cata['RA'], cata['Dec']])
     tree = KDTree(X)
     nd, ni = tree.query(X, k=11)
     ni = ni[:,1:]
     print(cata.iloc[ni[0]])
-    '''
+'''
 
 
 if args.folder is not None:
