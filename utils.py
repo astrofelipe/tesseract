c51924e403d767ac4374ebb92ed7f61c3294410f
astrofelipe <felipe.irh@gmail.com>
2019-03-27 01:16:22 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -70,9 +70,9 @@ def FFICut(ffis, x, y, size):
     x      = int(x)
     y      = int(y)
 
-    aflux  = ffis['FFIs'][:, x-size:x+size+1, y-size:y+size+1]
+    aflux  = ffis['FFIs'][:, x-size//2:x+size//2+1, y-size//2:y+size//2+1]
     print(aflux.shape)
-    aerrs  = ffis['errs'][:, x-size:x+size+1, y-size:y+size+1]
+    aerrs  = ffis['errs'][:, x-size//2:x+size//2+1, y-size//2:y+size//2+1]
 
     boxing = KeplerTargetPixelFileFactory(n_cadences=ncads, n_rows=size, n_cols=size)
 
