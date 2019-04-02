880c3985c3849b41257c1316287c307f4a4b6202
astrofelipe <felipe.irh@gmail.com>
2019-04-02 18:41:38 -0300

    Update generateTIC.py

--- generateTIC.py
+++ generateTIC.py
@@ -21,7 +21,7 @@ names = ['ID', 'version', 'HIP', 'TYC', 'UCAC', '2MASS', 'SDSS', 'ALLWISE', 'GAI
 
 files = glob.glob(args.Folder + 'tic_*.csv')
 
-df = pd.read_csv(files[0], names=names, index_col=False)
+df = pd.read_csv(files[0], names=names)
 print(df['eclong'])
 
 print(df.head())
