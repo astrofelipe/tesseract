75e72d6bbd8261546232d7eeb77b6516f9ad4690
astrofelipe <felipe.irh@gmail.com>
2019-04-16 15:40:52 -0400

    Update lite.py

--- lite.py
+++ lite.py
@@ -12,6 +12,7 @@ parser.add_argument('Targets', type=str)
 args = parser.parse_args()
 
 fs  = np.sort(glob.glob('/horus/TESS/FFI/s%04d/*.h5py' % args.Sector))
+print(fs)
 h5s = h5py.File(f for f in fs)
 
 if args.Targets[-3:] == 'pkl':
