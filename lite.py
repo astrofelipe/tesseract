1f772af3a8bb32dea3834a4b8436a21a150bc2da
astrofelipe <felipe.irh@gmail.com>
2019-04-16 15:32:31 -0400

    Update lite.py

--- lite.py
+++ lite.py
@@ -6,10 +6,24 @@ import numpy as np
 from joblib import Parallel, delayed
 
 parser = argparse.ArgumentParser(description='Generate lightcurves!')
-parser.add_argument('Sector', help='TESS Sector')
+parser.add_argument('Sector', type=int, help='TESS Sector')
+parser.add_argument('Targets', type='str')
 
 args = parser.parse_args()
 
 fs  = np.sort(glob.glob(args.Folder + '*.h5py'))
-print(fs)
 h5s = h5py.File(f for f in fs)
+
+if args.Targets[-3:] == 'pkl':
+    import pickle
+    f = open(args.Targets, 'rb')
+    d = pickle.load(f)
+    tics = d.keys()
+
+    sval = np.array([list(item.values()) for item in d.values()]).astype(bool)
+
+    print(tics)
+    print(sval)
+
+else:
+    tics = np.genfromtxt(args.Target, usecols=(0,), delim=',')
