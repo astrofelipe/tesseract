2061d713318735b2ac81ffd5232c9f58e761fbe7
astrofelipe <felipe.irh@gmail.com>
2019-05-08 14:57:46 -0400

    Update lite.py

--- lite.py
+++ lite.py
@@ -93,3 +93,5 @@ def make_lc(tic):
     inst   = np.repeat('TESS', len(time))
     output = np.transpose([time, lkf.flux, lkf.flux_err, inst])
     np.savetxt('TIC%s.dat' % (args.TIC), output, fmt='%s')
+
+make_lc(tics[0])
