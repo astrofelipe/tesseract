d98b9796b0b72ceb7880c515a31ea326bc870c9d
astrofelipe <felipe.irh@gmail.com>
2019-06-17 13:57:46 -0400

    Clean

--- FFI2h5.py
+++ FFI2h5.py
@@ -14,10 +14,13 @@ parser.add_argument('Camera', type=int)
 parser.add_argument('Chip', type=int)
 parser.add_argument('--ncpu', type=int, default=10)
 parser.add_argument('--nomemmap', action='store_false')
+parser.add_argument('--nstart', type=int, default=0)
+parser.add_argument('--nstop', type=int, default=None)
 args = parser.parse_args()
 
-files  = np.sort(glob.glob(args.Folder + '*%d-%d*.fits' % (args.Camera, args.Chip)))
-nfiles = len(files)
+allfiles  = np.sort(glob.glob(args.Folder + '*%d-%d*.fits' % (args.Camera, args.Chip)))
+files     = allfiles[args.nstart:args.nstop]
+nfiles    = len(allfiles)
 
 '''
 for i,f in enumerate(tqdm(files)):
@@ -67,8 +70,8 @@ dset   = output.create_dataset('FFIs', (nfiles, nx, ny), dtype='float64', compre
 derr   = output.create_dataset('errs', (nfiles, nx, ny), dtype='float64', compression='lzf')
 table  = output.create_dataset('data', (4, nfiles), dtype='float64', compression='lzf')
 
-dset[:] = Parallel(n_jobs=args.ncpu)(delayed(fits.getdata)(f, memmap=args.nomemmap) for f in tqdm(files))
-derr[:] = Parallel(n_jobs=args.ncpu)(delayed(fits.getdata)(f, 2, memmap=args.nomemmap) for f in tqdm(files))
+dset[args.nstart:args.nstop] = Parallel(n_jobs=args.ncpu)(delayed(fits.getdata)(f, memmap=args.nomemmap) for f in tqdm(files))
+derr[args.nstart:args.nstop] = Parallel(n_jobs=args.ncpu)(delayed(fits.getdata)(f, 2, memmap=args.nomemmap) for f in tqdm(files))
 
 table[:] = np.transpose(Parallel(n_jobs=args.ncpu)(delayed(make_table)(f) for f in tqdm(files)))
 
