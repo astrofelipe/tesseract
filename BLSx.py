15f97a097d6c340e5531d073c00ef7545d93624a
astrofelipe <felipe.irh@gmail.com>
2019-07-29 14:34:01 -0400

    Update BLSx.py

--- BLSx.py
+++ BLSx.py
@@ -27,10 +27,10 @@ if folder[-1] != '/':
 def run_BLS(fl):
     t, f = np.genfromtxt(fl, usecols=(0,1), unpack=True)
     #s10: 4913400--4913404 4913414.2--4913429
-    mask = ((t > 4913400) & (t < 4913403.5)) + ((t > 4913414.2) & (t < 4913417)) #s10
+    #mask = ((t > 4913400) & (t < 4913403.5)) + ((t > 4913414.2) & (t < 4913417)) #s10
     #mask = (f/np.median(f) > 0.985)
-    t = t[~mask]
-    f = f[~mask]
+    #t = t[~mask]
+    #f = f[~mask]
     #mask = (t > 2458492.) & ((t < 2458504.5) | (t > 2458505.))
     lc   = TessLightCurve(time=t, flux=f).flatten(window_length=21, polyorder=2, niters=3)
 
