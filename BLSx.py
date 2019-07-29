a16d455b7b6c8fc03f3b6fd7a3f57ac144ce18e1
astrofelipe <felipe.irh@gmail.com>
2019-07-29 13:38:10 -0400

    Update BLSx.py

--- BLSx.py
+++ BLSx.py
@@ -29,8 +29,8 @@ def run_BLS(fl):
     #s10: 4913400--4913404 4913414.2--4913429
     mask  = ((t > 4913400) + (t < 4913404)) & ((t > 4913414.2) + (t < 4913429)) #s10
     #mask = (f/np.median(f) > 0.985)
-    t = t[mask]
-    f = f[mask]
+    t = t[~mask]
+    f = f[~mask]
     #mask = (t > 2458492.) & ((t < 2458504.5) | (t > 2458505.))
     lc   = TessLightCurve(time=t, flux=f).flatten(window_length=21, polyorder=2, niters=3)
 
