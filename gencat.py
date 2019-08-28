4c6e0ae568f4592bfbb71146b951848bb064e58b
astrofelipe <felipe.irh@gmail.com>
2019-08-28 18:51:52 -0400

    Update gencat.py

--- gencat.py
+++ gencat.py
@@ -78,8 +78,8 @@ elo, ela = eclim[sec]
 print(elo,ela)
 
 #Not pole
-eclos = np.arange(elo[0], elo[1]+1.1, 4) % 360
-eclas = np.arange(ela[0], ela[1]+1.1, 4)
+eclos = np.arange(elo[0], elo[1]+1.1, 2) % 360
+eclas = np.arange(ela[0], ela[1]+1.1, 2)
 
 wrapcheck = np.any(np.diff(eclos) < 0)
 if wrapcheck:
