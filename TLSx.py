8459db57a6733cb3809f73180901c05812010fa7
astrofelipe <felipe.irh@gmail.com>
2019-10-24 03:53:39 -0300

    Update TLSx.py

--- TLSx.py
+++ TLSx.py
@@ -84,7 +84,7 @@ else:
     allfiles = glob.glob(args.Folder + 'TIC*.dat')
 
     results  = Table(Parallel(n_jobs=args.ncpu, verbose=0)(delayed(run_TLS)(f) for f in tqdm(allfiles[:100])))
-    print(results[:,5])
+    print(results)
     rmask    = results[:,5] != 99
     results  = results[rmask]
     #results  = np.array([run_TLS(f) for f in tqdm(allfiles)])
