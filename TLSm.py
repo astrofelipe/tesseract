17abe55a8565e628f2b010a644c23d43910880ef
astrofelipe <felipe.irh@gmail.com>
2019-11-10 12:03:17 -0300

    Update TLSm.py

--- TLSm.py
+++ TLSm.py
@@ -1,8 +1,15 @@
 import glob
 import argparse
 import numpy as np
+from TLSx import the_TLS
 
 parser = argparse.ArgumentParser(description='Run TLS over selected multisector LCs')
+parser.add_argument('Folder', type=str, help='Folder with LCs (organized by sector and magnitude)')
 parser.add_argument('File', type=str, help='Multisector list')
+parser.add_argument('--target', type=int, default=None, help='Runs on single target')
 
 args = parser.parse_args()
+
+if args.target:
+    fns = glob.glob(args.Folder + '*/*/TIC%d.dat' % args.target)
+    print(fns)
