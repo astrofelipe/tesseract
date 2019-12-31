f949e9d2fc1071c9a7cabeafb3d915122f375d02
astrofelipe <felipe.irh@gmail.com>
2019-12-31 02:33:18 -0300

    Update BLSx_check.py

--- BLSx_check.py
+++ BLSx_check.py
@@ -38,7 +38,7 @@ nlc = 7
 for i in range(args.start, len(BLSdata)):
     print('\nIteration: ',i)
 
-    fig = plt.figure(constrained_layout=True, figsize=[21, 1.5*nlc], dpi=72)
+    fig = plt.figure(constrained_layout=True, figsize=[22, 1.5*nlc], dpi=80)
     gs  = GridSpec(ncols=9, nrows=nlc, figure=fig, width_ratios=[9,2,2,1,0.5,9,2,2,1])
 
     lcs = np.ravel([fig.add_subplot(gs[k%nlc, 5*(k//nlc)]) for k in range(2*nlc)])
@@ -108,6 +108,6 @@ for i in range(args.start, len(BLSdata)):
     chunk['depth'].format    = '%d'
 
     print(chunk)
-    gs.tight_layout(fig)
+    #gs.tight_layout(fig)
     plt.show()
     plt.close(fig)
