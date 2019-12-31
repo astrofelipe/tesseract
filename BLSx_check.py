4ad74bf19db616d36bc54745831cfe45ea12b9dd
astrofelipe <felipe.irh@gmail.com>
2019-12-31 02:52:44 -0300

    Update BLSx_check.py

--- BLSx_check.py
+++ BLSx_check.py
@@ -97,8 +97,8 @@ for i in range(args.start, len(BLSdata)):
 
         def on_press(event):
             #chunk['Files', 'P', 't0', 'duration', 'depth'][j].pprint(show_name=False, align='<')
-            hey = chunk[j]['Files', 'P', 't0', 'duration', 'depth']
-            print(hey.pformat())
+            hey = np.array(chunk['Files', 'P', 't0', 'duration', 'depth'][j])
+            print(hey)
 
         funcs.append(on_press)
 
