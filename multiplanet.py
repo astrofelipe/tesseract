cd623a8f7e113c066c26b8c525c007f91bba7b3e
astrofelipe <felipe.irh@gmail.com>
2020-01-04 21:59:59 -0300

    Update multiplanet.py

--- multiplanet.py
+++ multiplanet.py
@@ -57,17 +57,17 @@ for i in range(args.nplanets):
         depth  = result.depth[idx]
 
     #if i==1:
-    #    period *= 2
+    #    period *= 2 2458339.018159
 
     phase = (lc.time - t0 + 0.5*period) % period - 0.5*period
     fph   = (t - t0 + 0.5*period) % period - 0.5*period
 
     ax = fig.add_subplot(gs[1,i])
     ax.plot(fph, f, '.', color=color[i])
-    axlc.plot(result.transit_times, depth*np.ones(len(result.transit_times)), '^', color=color[i])
+    axlc.plot(result.transit_times, depth*np.ones(len(result.transit_times)) - 2000e-6, '^', color=color[i])
 
     ax.set_xlim(-2*dur, 2*dur)
-    ax.set_title(r'$P=%f$ / $T_0=%f' % (period,t0))
+    ax.set_title(r'$P=%f$ / $T_0=%f$' % (period,t0))
 
     ax2 = fig.add_subplot(gs[2,i])
     ax2.plot(periods, power, '-', lw=1)
