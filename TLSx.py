99461ed6762f8a059a1a288dd80c7cac8a2ae6f3
astrofelipe <felipe.irh@gmail.com>
2020-01-08 17:43:56 -0300

    Update TLSx.py

--- TLSx.py
+++ TLSx.py
@@ -8,18 +8,17 @@ from transitleastsquares import transitleastsquares as TLS
 
 def run_TLS(fn, min_period=0.2, target=None):
     t,f,e = np.genfromtxt(fn, usecols=(0,1,2), unpack=True)
-    lc    = TessLightCurve(time=t, flux=f, flux_err=e).flatten(window_length=51, polyorder=2, niters=5)
-
-    return the_TLS(fn, lc.time, lc.flux, lc.flux_err, min_period, target)
-
-def the_TLS(fn, t, f, e, min_period=0.2, target=None, use_threads=1, show_progress_bar=False):
     mask  = cleaner(t, f)
 
     t = t[~mask]
     f = f[~mask]
     e = e[~mask]
 
-    #try:
+    lc = TessLightCurve(time=t, flux=f, flux_err=e).flatten(window_length=51, polyorder=2, niters=5)
+
+    return the_TLS(fn, lc.time, lc.flux, lc.flux_err, min_period, target)
+
+def the_TLS(fn, t, f, e, min_period=0.2, target=None, use_threads=1, show_progress_bar=False):
     try:
         model   = TLS(t, f, e) #Algunos targets pueden tener flujo negativo (borde FFI) y se cae
     except:
