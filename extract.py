3cb5201002419f2717cf61e410d537aa9443fa49
astrofelipe <felipe.irh@gmail.com>
2020-01-10 14:30:30 -0300

    Update extract.py

--- extract.py
+++ extract.py
@@ -8,16 +8,19 @@ parser.add_argument('File', help='Light curve file (.FITS)')
 args     = parser.parse_args()
 filename = args.File
 
-data = fits.getdata(filename)
-head = fits.getheader(filename, 1)
-print(head)
+data  = fits.getdata(filename)
+head  = fits.getheader(filename, 0)
+head2 = fits.getheader(filename, 1)
 
 TIC  = head['TICID']
 sec  = head['SECTOR']
+bjdr = head2['BJDREFI']
 q    = data['QUALITY'] == 0
-t    = data['TIME'][q]
+t    = data['TIME'][q] + bjdr
 f    = data['PDCSAP_FLUX'][q]
 e    = data['PDCSAP_FLUX_ERR'][q]
+
+print(data['PDCSAP_FLUX'])
 
 output = np.transpose((t,f,e))
 np.savetxt('TIC%d_%02d-SC.dat' % (TIC, sec), output, fmt='%s')
