891ecbcea9cb57e347dc6bcc25629bc149d434a4
astrofelipe <felipe.irh@gmail.com>
2020-01-10 14:23:28 -0300

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,22 @@
+from astropy.io import fits
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='Converts TESS official LCs to Juliet format')
+parser.add_argument('File', help='Light curve file (.FITS)')
+
+args     = parser.parse_args()
+filename = args.File
+
+data = fits.getdata(filename)
+head = fits.getgeader(filename)
+
+TIC  = head['TICID']
+sec  = head['SECTOR']
+q    = data['QUALITY'] == 0
+t    = data['TIME'][q]
+f    = data['PDCSAP_FLUX'][q]
+e    = data['PDCSAP_FLUX_ERR'][q]
+
+output = np.transpose((t,f,e))
+np.savetxt('TIC%d_%02d-SC.dat' % (TIC, sec), output)
