27b9d202f652e34165ded5298ec001387e3177a9
astrofelipe <felipe.irh@gmail.com>
2020-01-21 15:48:58 -0300

    Update multiplanet.py

--- multiplanet.py
+++ multiplanet.py
@@ -21,7 +21,7 @@ t,f,e = np.genfromtxt(args.File, usecols=(0,1,2), unpack=True)
 ma    = cleaner(t, f)
 
 t,f,e = t[~ma], f[~ma], e[~ma]
-wl    = 3*(int(np.sqrt(len(t)))//2)+1
+wl    = 4*(int(np.sqrt(len(t)))//2)+1
 lc    = TessLightCurve(time=t, flux=f, flux_err=e).flatten(window_length=wl, polyorder=2, niters=3)
 
 fig = plt.figure(constrained_layout=True, figsize=[15, 6])
