f1bdc3a87c2700e9139981ad6a26067bf4cd5c7e
astrofelipe <felipe.irh@gmail.com>
2020-03-09 15:49:43 -0300

    Update lcplot.py

--- lcplot.py
+++ lcplot.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='LC and transit plot (useful for TTV priors!)')
-parser.add_argument('Light curves', type=str, nargs='+', help='Light curve file(s)')
+parser.add_argument('LCFiles', type=str, nargs='+', help='Light curve file(s)')
 parser.add_argument('--transit', type=float, nargs=3, help='P and t0 values')
 
 args = parser.parse_args()
 
 fig, ax = plt.subplots(figsize=[15,3])
 
-for f in args.Lightcurves:
+for f in args.LCFiles:
     print(f)
