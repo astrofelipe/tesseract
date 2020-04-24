47ad1c16e80cc8729c9dc96ba66046761eafff8b
astrofelipe <felipe.irh@gmail.com>
2020-04-24 17:28:08 -0400

    Update OoT.py

--- OoT.py
+++ OoT.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import welch
 from astropy.timeseries import LombScargle
+from astropy.table import Table
 
 parser = argparse.ArgumentParser(description='Calculates the PSD for the out of transit light curve')
 parser.add_argument('File', help='File containing the light curve')
@@ -13,13 +14,14 @@ parser.add_argument('--dur', type=float, help='x1.5 Duration of the transit', de
 args = parser.parse_args()
 
 
-t,f,e = np.genfromtxt(args.File, usecols=(0,1,2), unpack=True)
-insts = np.genfromtxt(args.File, usecols=(3,), dtype=str)
-print(insts)
+#t,f,e = np.genfromtxt(args.File, usecols=(0,1,2), unpack=True)
+#insts = np.genfromtxt(args.File, usecols=(3,), dtype=str)
+data = Table.read(args.File)
+print(data)
 phase = (t - args.t0 + 0.5*args.P) % args.P - 0.5*args.P
 mask  = np.abs(phase) > 3*args.dur
 
-to, fo, eo = t[mask], f[mask], e[mask]
+data2 = data[mask]
 
 '''
 fig, ax = plt.subplots(figsize=[10,3])
@@ -49,5 +51,4 @@ ax.plot(pers, pow, '-k')
 plt.show()
 '''
 
-data = np.transpose([to,fo,eo, insts], dtype=str)
-print(data)
+print(data2)
