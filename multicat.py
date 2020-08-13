482be4a1cd60a2f0ca66328fea0277a9b611222e
astrofelipe <felipe.irh@gmail.com>
2020-08-13 05:12:37 -0400

    Update multicat.py

--- multicat.py
+++ multicat.py
@@ -22,6 +22,5 @@ else:
 catfiles = [args.Folder + 's%04d/s%04d_%d.000000-%d.000000.csv' % (s,s,minmag,maxmag) for s in range(minsec,maxsec+1)]
 catalogs = [pd.read_csv(f, names=['ID', 'ra', 'dec', 'Tmag'], skiprows=1) for f in catfiles]
 concat   = pd.concat(catalogs)
-df       = concat.groupby(['ID']).value_counts().to_frame('count')
-
+df       = concat.pivot_table(index=['ID'], aggfunc='size')
 print(df)
