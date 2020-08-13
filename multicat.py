22d6095ca0f4f3c8c507ef28335b6d2960e6d890
astrofelipe <felipe.irh@gmail.com>
2020-08-13 05:42:55 -0400

    Update multicat.py

--- multicat.py
+++ multicat.py
@@ -23,5 +23,6 @@ catfiles = [args.Folder + 's%04d/s%04d_%d.000000-%d.000000.csv' % (s,s,minmag,ma
 catalogs = [pd.read_csv(f, names=['ID', 'ra', 'dec', 'Tmag'], skiprows=1) for f in catfiles]
 concat   = pd.concat(catalogs)
 counter  = concat.pivot_table(columns=['ID'], aggfunc='size')
-print(counter)
-#df = counter.query('')
+
+df = counter.query('ID > 1')
+print(df)
