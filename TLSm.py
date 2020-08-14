5a3bd80d3e01ede3e55cb09ae7edd4485a6c3c74
astrofelipe <felipe.irh@gmail.com>
2020-08-13 21:42:56 -0400

    Update TLSm.py

--- TLSm.py
+++ TLSm.py
@@ -7,7 +7,7 @@ from cleaner import cleaner
 
 parser = argparse.ArgumentParser(description='Run TLS over selected multisector LCs')
 parser.add_argument('Folder', type=str, help='Folder with LCs (organized by sector and magnitude)')
-parser.add_argument('File', type=str, help='Multisector list')
+#parser.add_argument('File', type=str, help='Multisector list')
 parser.add_argument('--target', type=int, default=None, help='Runs on single target')
 
 args = parser.parse_args()
@@ -15,7 +15,7 @@ args = parser.parse_args()
 subfolder = args.File.split('_')[-1].split('.')[0]
 
 if args.target:
-    fns = glob.glob(args.Folder + '*/%s/TIC%d.dat' % (subfolder, args.target))
+    fns = glob.glob(args.Folder + '*/*/TIC%d.dat' % args.target)
 
     t,f,e = [],[],[]
 
