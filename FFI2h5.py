9fb48504da62cd9bc51745abe61c2d72845a759b
astrofelipe <felipe.irh@gmail.com>
2020-10-17 20:02:50 -0300

    Update FFI2h5.py

--- FFI2h5.py
+++ FFI2h5.py
@@ -53,8 +53,8 @@ nx, ny = fits.getdata(files[0]).shape
 output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
 #output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
 
-dset   = output.create_dataset('FFIs', (nx, ny, nfiles), dtype='float64', chunks=(48, 48, 1), compression='lzf')
-derr   = output.create_dataset('errs', (nx, ny, nfiles), dtype='float64', chunks=(48, 48, 1), compression='lzf')
+dset   = output.create_dataset('FFIs', (nx, ny, nfiles), dtype='float64', chunks=(32, 32, nfiles), compression='lzf')
+derr   = output.create_dataset('errs', (nx, ny, nfiles), dtype='float64', chunks=(32, 32, nfiles), compression='lzf')
 table  = output.create_dataset('data', (4, nfiles), dtype='float64', compression='lzf')
 
 #dset[args.nstart:args.nstop] = Parallel(n_jobs=args.ncpu)(delayed(get_data)(f) for f in tqdm(files))
