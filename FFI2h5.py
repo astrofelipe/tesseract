955bf6da034bd486d95fc868eee6a88c3e7862c6
astrofelipe <felipe.irh@gmail.com>
2020-10-19 21:12:42 -0300

    Update FFI2h5.py

--- FFI2h5.py
+++ FFI2h5.py
@@ -46,8 +46,8 @@ nx, ny = fits.getdata(files[0]).shape
 #output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
 output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
 
-dset   = output.create_dataset('FFIs', (nx, ny, nfiles), dtype='float64')#, chunks=(32, 32, nfiles//10), compression='lzf')
-derr   = output.create_dataset('errs', (nx, ny, nfiles), dtype='float64')#, chunks=(32, 23, nfiles//10), compression='lzf')
+dset   = output.create_dataset('FFIs', (nx, ny, nfiles), dtype='float64', chunks=(32, 32, nfiles//10))#, compression='lzf')
+derr   = output.create_dataset('errs', (nx, ny, nfiles), dtype='float64', chunks=(32, 23, nfiles//10))#, compression='lzf')
 table  = output.create_dataset('data', (4, nfiles), dtype='float64')#, compression='lzf')
 
 for i,f in enumerate(tqdm(files)):
