189a42b41704e38467ab436e0386fc2e01144b53
astrofelipe <felipe.irh@gmail.com>
2020-10-19 21:21:16 -0300

    Update FFI2h5.py

--- FFI2h5.py
+++ FFI2h5.py
@@ -43,8 +43,8 @@ def make_table(f):
 
 nx, ny = fits.getdata(files[0]).shape
 
-#output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
-output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
+output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
+#output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
 
 dset   = output.create_dataset('FFIs', (nx, ny, nfiles), dtype='float64', chunks=True)#, chunks=(32, 32, nfiles//10))#, compression='lzf')
 derr   = output.create_dataset('errs', (nx, ny, nfiles), dtype='float64', chunks=True)#, chunks=(32, 23, nfiles//10))#, compression='lzf')
