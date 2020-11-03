a917bdb31b3560e29f1d08ef3f9040ffc566e094
astrofelipe <felipe.irh@gmail.com>
2020-11-02 23:26:57 -0300

    Update lite.py

--- lite.py
+++ lite.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
-mpl.use('Agg')
+mpl.use('GTKAgg')
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from utils import pixel_border
