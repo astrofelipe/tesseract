81b7dd1ae7c7e811fcecb001e983cf8593101272
astrofelipe <firojas@uc.cl>
2021-04-06 22:41:41 -0400

    cat

--- gencat.py
+++ gencat.py
@@ -154,7 +154,8 @@ if (args.Sector < 14) or (args.Sector > 26):
 
 
     print(supercata1, supercata2)
-    print(supercata1.keys(), supercata2.keys())
+    help(supercata)
+    print(supercata1.keys, supercata2.keys())
     supercata = vstack([supercata1, supercata2])#, silent=True)
 
 else:
