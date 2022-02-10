bf4ca1d0721b63cd2a9a63227c849306ef7dbb66
astrofelipe <firojas@uc.cl>
2022-02-10 18:13:51 -0300

    mpi?

--- FFI2h5.py
+++ FFI2h5.py
@@ -46,9 +46,9 @@ nx, ny = fits.getdata(files[0]).shape
 #output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
 output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', driver='mpio', comm=MPI.COMM_WORLD)
 
-dset   = output.create_dataset('FFIs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='lzf')
-derr   = output.create_dataset('errs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='lzf')
-table  = output.create_dataset('data', (4, nfiles), dtype='float64', compression='lzf')
+dset   = output.create_dataset('FFIs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='gzip')
+derr   = output.create_dataset('errs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='gzip')
+table  = output.create_dataset('data', (4, nfiles), dtype='float64', compression='gzip')
 print(dset.chunks, derr.chunks)
 
 for i,f in enumerate(tqdm(files)):
