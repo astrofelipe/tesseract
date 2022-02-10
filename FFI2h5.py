5251db7422fe8de3c79a88c77cee4319a283b9ba
astrofelipe <firojas@uc.cl>
2022-02-10 18:08:53 -0300

    mpi?

--- FFI2h5.py
+++ FFI2h5.py
@@ -43,8 +43,8 @@ def make_table(f):
 
 nx, ny = fits.getdata(files[0]).shape
 
-output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
-#output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
+#output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest')
+output = h5py.File('TESS-FFIs_s%04d-%d-%d.hdf5' % (args.Sector, args.Camera, args.Chip), 'w', libver='latest', driver='mpio', comm=MPI.COMM_WORLD)
 
 dset   = output.create_dataset('FFIs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='lzf')
 derr   = output.create_dataset('errs', (nfiles, nx, ny), dtype='float64', chunks=(64, 24, 24), compression='lzf')
@@ -57,9 +57,9 @@ for i,f in enumerate(tqdm(files)):
         dat1 = hdu[1].data
         dat2 = hdu[2].data
 
-        dset[i:i+1] = np.expand_dims(dat1, axis=0)
-        derr[i:i+1] = np.expand_dims(dat2, axis=0)
-        table[:,i] = make_table(f)
+        with dset.collective: dset[i:i+1] = np.expand_dims(dat1, axis=0)
+        with derr.collective: derr[i:i+1] = np.expand_dims(dat2, axis=0)
+        with table.collective: table[:,i] = make_table(f)
 
         hdu.close()
         del hdu, dat1, dat2
