e7fb4812dde26370dea696bcc32a713c1e7c665b
astrofelipe <firojas@uc.cl>
2022-05-23 10:47:23 -0400

    stretch

--- ticlc.py
+++ ticlc.py
@@ -18,7 +18,7 @@ from astropy.coordinates import SkyCoord
 from astropy.stats import SigmaClip, mad_std
 from astropy.wcs import WCS
 from astropy.io import fits
-from astropy.visualization import MinMaxInterval, SinhStretch, ImageNormalize, AsymmetricPercentileInterval, MinMaxInterval
+from astropy.visualization import MinMaxInterval, SinhStretch, ImageNormalize, AsymmetricPercentileInterval, MinMaxInterval, ContrastBiasStretch
 from tess_stars2px import tess_stars2px_function_entry as ts2p
 
 parser = argparse.ArgumentParser(description='Extract Lightcurves from FFIs')
@@ -528,7 +528,7 @@ if args.pngstamp is not None:
     #                       extent=textent)
 
     mstamp    = np.nanmedian(flux[::10], axis=0)
-    norm      = ImageNormalize(mstamp, interval=MinMaxInterval(), stretch=SinhStretch())
+    norm      = ImageNormalize(mstamp, interval=MinMaxInterval(), stretch=ContrastBiasStretch())
     stamp     = sax.imshow(mstamp,
                            cmap=lcmap, origin='lower', aspect='equal', alpha=talpha,
                            extent=textent, norm=norm)
